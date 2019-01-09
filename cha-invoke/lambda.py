c4aefd1e3dbbf72e7d56e3e1c8a659678dc80cf0
Shiyar Daoud <daoud@axxessio.com>
2019-01-09 08:18:54 +0100

    initial import

---
+++ cha-invoke/lambda.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+
+
+client = boto3.client('lambda')
+event = {
+    "path": "/",
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "resource": "/",
+    "httpMethod": "GET",
+    "queryStringParameters": None,
+    "pathParameters": None,
+    "stageVariables": None,
+    "body": ""
+}
+
+response = client.invoke(
+    FunctionName='cha-lambda-dev',
+    InvocationType='RequestResponse',
+    Payload=bytes(json.dumps(event), 'utf-8'),
+    Qualifier='$LATEST'
+)
+res = json.loads(response['Payload'].read().decode("utf-8"))
+print(res)
